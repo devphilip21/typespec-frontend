543dd279d6dfc763f2de577e40020bd4fc08cec9
devphilip21 <philip21.dev@gmail.com>
2025-05-13 18:28:27 +0900

    feat(type-checker): add isDeclarationType function

--- packages/compiler-helper/src/type-checker.ts
+++ packages/compiler-helper/src/type-checker.ts
@@ -1,4 +1,5 @@
 import { Program, Type } from '@typespec/compiler';
+import { $ } from '@typespec/compiler/typekit';
 
 export function isBuiltInType(program: Program, type: Type) {
   if (!('namespace' in type) || type.namespace === undefined) {
@@ -16,4 +17,29 @@ export function isBuiltInType(program: Program, type: Type) {
   }
 
   return tln === globalNs.namespaces.get('TypeSpec');
+}
+
+export function isDeclarationType(program: Program, type: Type): boolean {
+  switch (type.kind) {
+    case 'Namespace':
+    case 'Interface':
+    case 'Operation':
+    case 'EnumMember':
+    case 'UnionVariant':
+      return false;
+    case 'Model':
+      if (($(program).array.is(type) || $(program).record.is(type)) && isBuiltInType(program, type)) {
+        return false;
+      }
+
+      return Boolean(type.name);
+    case 'Union':
+      return Boolean(type.name);
+    case 'Enum':
+      return true;
+    case 'Scalar':
+      return true;
+    default:
+      return false;
+  }
 }
